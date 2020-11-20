9c1563bca9e6d77971749c1ca2fa30bfd8315d24
Keefe Brown <keefe14brown@gmail.com>
2020-11-19 21:32:25 -0500

    didn't hit mvp yet, but I did get some good things done. will have more updated before AM meeting.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const carouselNext = document.querySelector('.carousel-next');
 const carouselPrevious = document.querySelector('.carousel-previous');
 const carouselSelect = document.querySelector('.carousel-select');
 const workButton = document.querySelector('.workbutton');
+const propertyButton = document.querySelector('.buy');
 
 //////BUILD MODAL AND CAROUSEL///////  
 const backgroundImage = [
@@ -57,7 +58,24 @@ const openCarousel = () => {
 
 // ======================================================= \\
 //////***/ CREATE CLASS CONSTRUCTORS FOR THE CHARACTERS***\\\\
-//////***/ AND THE CHRACTER CAREERS\***\\\\\\\\\\\\\\\\\\\\\\\\
+//////***/ AND THE CHRACTER CAREERS/ATTRIBUTES. \***\\\\\\\\\\\\\\\\\\\\\\\\
+
+class player {
+    constructor(name, career, property, wallet){
+        this.name = name;
+        this.career = career;
+        this.property = "";
+        this.wallet = 0;
+    }
+} 
+
+class property {
+    constructor(type, price, value){
+        this.type = type;
+        this.price = price;
+        this.value = value;
+    }
+}
 
 class Career {
     constructor(name, description, income, id){
@@ -69,11 +87,11 @@ class Career {
 }
 const careerDescriptions = [
     {
-        name: 'Unsigned Rapper',
+        name: 'Independent Rapper',
         description: 'Making music and performing local gigs for money.'
     },
     {
-        name: 'Software-Engineer',
+        name: 'Senior Software-Engineer',
         description: 'solves software related problems and build application architecture.'
     },
     {
@@ -81,8 +99,8 @@ const careerDescriptions = [
         description: 'Helping people with their medical needs.'
     },
     {
-        name: 'Youtube Star',
-        description: 'Talk about stuff on social media and people say wow and i get paid'
+        name: 'Tik-Tok Star',
+        description: 'Make videos doing corny dance routines, and voice-overs'
     }
 ]
 const careerIncomes = [
@@ -96,7 +114,63 @@ const careerIncomes = [
 ];
 const careers = [];
 
+const playerOne = new player('Joe Shmoe', 0, "", 0)
+const playerTwo = new player('player 2', 0, 0, 0)
 
+player
+
+const Car = new property('New Convertible', 40000, 25000)
+const house = new property('Buckhead Mansion', 1000000, 1000000)
+
+////=======CHARACTER STAT FUNCTIONS===========\\\\
+//////////////////////////////////////////////////
+const randomization = (limit) => {
+    return Math.floor(Math.random() * limit)
+   }
+
+   for (let i = 7 ; i > 0; i--){
+    const job = careerDescriptions[randomization(careerDescriptions.length)]
+    const income = careerIncomes[randomization(careerIncomes.length)];
+    careers.push(new Career(job.name, job.description, income, `${job.name}-${income}` ))
+   }
+
+   playerOne.career = careers[randomization(careers.length)]
+   playerTwo.career = careers[randomization(careers.length)]
+
+   playerOne.el = document.getElementById('Joe Schmoe');
+
+playerOne.render = () => {
+    playerOne.el.innerHTML = `
+    <h1>${playerOne.name}</h1>
+    <h3> Current skill: ${playerOne.career.name} </h3>
+    <h3>${playerOne.name} current income: ${playerOne.career.income}</h3>
+    <h3> Currently ${playerOne.name} has ${playerOne.wallet} in the bank</h3>
+    <div> <h2>Properties:</h2> 
+    <ul>${playerOne.property}
+    <li>${playerOne.name} currently owns a: ${property.type} <br>
+    Current worth: ${property.value} 
+            </li></ul>
+    </div>`;
+}
+
+playerOne.render();
+
+const goToWorkButton = document.querySelector('.gotowork');
+   goToWorkButton.addEventListener('click', () => {
+    playerOne.wallet += playerOne.career.income;
+    playerOne.render();
+})
+
+propertyButton.addEventListener('click', ()=>{
+    if(barbie.wallet >= property.price){
+        playerOne.property.push(playerOne.property);
+        playerOne.wallet = playerOne.wallet - property.price;
+        barbie.render();
+        
+    } else {
+        alert("You know you don't have enough bread! cut it out!");
+    }
+})
 
 /* =============================
         EVENT LISTENERS
